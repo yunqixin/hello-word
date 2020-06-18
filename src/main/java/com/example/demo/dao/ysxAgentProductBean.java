0d9313595af901b1eed79abca9d724a818e7e2e2
yuanqixin <b46a1cae744ce990eb3e6bb1c1a57873>
2020-06-18 15:02:25 +0800

    logs

---
+++ src/main/java/com/example/demo/dao/ysxAgentProductBean.java
@@ -0,0 +1,87 @@
+package com.example.demo.dao;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class ysxAgentProductBean {
+
+    private Long agentProductId;
+
+    private String productName;
+
+    private String productCover;
+
+    private Short productType;
+
+    private Long productId;
+
+    private String saleUrl;
+
+    private String salePoster;
+
+    private Integer width;
+
+    private Integer height;
+
+    private Integer iconSize;
+
+    private Date createDate;
+
+    private Long createUserId;
+
+    private BigDecimal incomeProportion;
+
+    private BigDecimal seniorProportion;
+
+    private BigDecimal seniorAmount;
+
+    private BigDecimal incomeAmount;
+
+    private BigDecimal productPrice;
+
+    private BigDecimal salePrice;
+
+    private BigDecimal oneselfPrice;
+
+    private Byte sortCode;
+
+    private Byte status;
+
+    private String iconUrl;
+
+    private Integer iconWidth;
+
+    private Integer iconHeight;
+
+    private Integer textWidth;
+
+    private Integer textHeight;
+
+    private Integer textFront;
+
+    private String shareTitle;
+
+    private String shareDetailes;
+
+    private Byte defaultShare;
+
+    private Byte channelType;
+
+    private String shareImg;
+
+    private Byte productPackageType;
+
+    private String remark;
+
+    private String productDesc;
+
+    private String productUnit;
+
+    private BigDecimal highestPrice;
+
+    private Byte shelfStatus;
+
+    private Byte mallLogo;
+
+    private String sharePoster;
+}
